932b5ce413c30692dbfc65704b8e343b4b4538f4
MikhailApsalikov <dee_jay@bk.ru>
2015-06-21 20:47:12 +0300

    Test example changed again

--- Kazakova.TestSystem.Logic/Fake/FakeTester.cs
+++ Kazakova.TestSystem.Logic/Fake/FakeTester.cs
@@ -15,48 +15,22 @@
 			this.fileContent = fileContent;
 		}
 
-		public BidirectionalGraph<object, IEdge<object>> GetBidirectionGraphForWholeGraph() // TODO
+		public BidirectionalGraph<object, IEdge<object>> GetBidirectionGraphForWholeGraph()
 		{
 			var result = new BidirectionalGraph<object, IEdge<object>>();
 			result.AddVertex("Начало");
-			for (var i = 1; i <= 19; i++)
+			for (var i = 1; i <= 04; i++)
 			{
 				result.AddVertex(i.ToString("D2"));
 			}
 			result.AddVertex("Конец");
+			
 			result.AddEdge(new Edge<object>("Начало", "01"));
 			result.AddEdge(new Edge<object>("01", "02"));
 			result.AddEdge(new Edge<object>("02", "03"));
-			result.AddEdge(new Edge<object>("02", "06"));
-			result.AddEdge(new Edge<object>("02", "12"));
-			result.AddEdge(new Edge<object>("02", "16"));
-			result.AddEdge(new Edge<object>("06", "07"));
-			result.AddEdge(new Edge<object>("07", "08"));
-			result.AddEdge(new Edge<object>("07", "10"));
-			result.AddEdge(new Edge<object>("08", "09"));
-			result.AddEdge(new Edge<object>("09", "19"));
-			result.AddEdge(new Edge<object>("10", "11"));
-			result.AddEdge(new Edge<object>("11", "19"));
-
-			result.AddEdge(new Edge<object>("15", "19"));
-			result.AddEdge(new Edge<object>("14", "12"));
-			result.AddEdge(new Edge<object>("12", "15"));
-			result.AddEdge(new Edge<object>("14", "15"));
-			result.AddEdge(new Edge<object>("13", "14"));
-			result.AddEdge(new Edge<object>("12", "13"));
-
+			result.AddEdge(new Edge<object>("02", "04"));
 			result.AddEdge(new Edge<object>("03", "04"));
-			result.AddEdge(new Edge<object>("04", "03"));
-			result.AddEdge(new Edge<object>("04", "05"));
-			result.AddEdge(new Edge<object>("03", "05"));
-			result.AddEdge(new Edge<object>("05", "19"));
-
-			result.AddEdge(new Edge<object>("16", "17"));
-			result.AddEdge(new Edge<object>("17", "18"));
-			result.AddEdge(new Edge<object>("16", "18"));
-			result.AddEdge(new Edge<object>("18", "19"));
-
-			result.AddEdge(new Edge<object>("19", "Конец"));
+			result.AddEdge(new Edge<object>("04", "Конец"));
 			return result;
 		}
 
@@ -65,9 +39,9 @@
 			switch (criteria)
 			{
 				case Criteries.OperatorsCover:
-					return 5;
+					return 1;
 				default:
-					return 6;
+					return 2;
 			}
 		}
 
@@ -80,15 +54,7 @@
 					switch (pathId)
 					{
 						case 0:
-							return GetGraphByPathVertexes(1, 2, 3, 4, 5, 19);
-						case 1:
-							return GetGraphByPathVertexes(1, 2, 6, 7, 8, 9, 19);
-						case 2:
-							return GetGraphByPathVertexes(1, 2, 6, 7, 10, 11, 19);
-						case 3:
-							return GetGraphByPathVertexes(1, 2, 12, 13, 14, 15, 19);
-						case 4:
-							return GetGraphByPathVertexes(1, 2, 16, 17, 18, 19);
+							return GetGraphByPathVertexes(1, 2, 3, 4);
 						default:
 							throw new ArgumentException("Данного пути не существует");
 					}
@@ -98,38 +64,33 @@
 					switch (pathId)
 					{
 						case 0:
-							return GetGraphByPathVertexes(1, 2, 3, 4, 5, 19);
+							return GetGraphByPathVertexes(1, 2, 3, 4);
 						case 1:
-							return GetGraphByPathVertexes(1, 2, 6, 7, 8, 9, 19);
-						case 2:
-							return GetGraphByPathVertexes(1, 2, 6, 7, 10, 11, 19);
-						case 3:
-							return GetGraphByPathVertexes(1, 2, 12, 13, 14, 15, 19);
-						case 4:
-							return GetGraphByPathVertexes(1, 2, 16, 17, 18, 19);
-						case 5:
-							return GetGraphByPathVertexes(1, 2, 16, 18, 19);
+							return GetGraphByPathVertexes(1, 2, 4);
 						default:
 							throw new ArgumentException("Данного пути не существует");
 					}
 				}
 				case Criteries.ConditionsCover:
+				{
+					switch (pathId)
+					{
+						case 0:
+							return GetGraphByPathVertexes(1, 2, 4);
+						case 1:
+							return GetGraphByPathVertexes(1, 2, 4);
+						default:
+							throw new ArgumentException("Данного пути не существует");
+					}
+				}
 				case Criteries.SolutionsAndConditionsCover:
 				{
 					switch (pathId)
 					{
 						case 0:
-							return GetGraphByPathVertexes(1, 2, 3, 4, 5, 19);
+							return GetGraphByPathVertexes(1, 2, 3, 4);
 						case 1:
-							return GetGraphByPathVertexes(1, 2, 6, 7, 8, 9, 19);
-						case 2:
-							return GetGraphByPathVertexes(1, 2, 6, 7, 10, 11, 19);
-						case 3:
-							return GetGraphByPathVertexes(1, 2, 12, 13, 14, 15, 19);
-						case 4:
-							return GetGraphByPathVertexes(1, 2, 16, 17, 18, 19);
-						case 5:
-							return GetGraphByPathVertexes(1, 2, 16, 18, 19);
+							return GetGraphByPathVertexes(1, 2, 4);
 						default:
 							throw new ArgumentException("Данного пути не существует");
 					}
@@ -141,7 +102,7 @@
 			}
 		}
 
-		public string GetRequiredParametersForPathAsString(Criteries criteria, int pathId) // TODO
+		public string GetRequiredParametersForPathAsString(Criteries criteria, int pathId)
 		{
 			switch (criteria)
 			{
@@ -150,15 +111,7 @@
 						switch (pathId)
 						{
 							case 0:
-								return "level = 1";
-							case 1:
-								return "factor = 6; level = 2";
-							case 2:
-								return "factor = -100; level = 2";
-							case 3:
-								return "level = 3";
-							case 4:
-								return "level = 100; factor = 0; k = 11";
+								return "a = 0; b = 100";
 							default:
 								throw new ArgumentException("Данного пути не существует");
 						}
@@ -168,17 +121,9 @@
 						switch (pathId)
 						{
 							case 0:
-								return "level = 1";
+								return "a = 0; b = 100";
 							case 1:
-								return "factor = 6; level = 2";
-							case 2:
-								return "factor = -100; level = 2";
-							case 3:
-								return "level = 3";
-							case 4:
-								return "level = 100; factor = 0; k = -100";
-							case 5:
-								return "level = 100; factor = 2; k = -100";
+								return "a = 1; b = 100";
 							default:
 								throw new ArgumentException("Данного пути не существует");
 						}
@@ -188,17 +133,9 @@
 						switch (pathId)
 						{
 							case 0:
-								return "level = 1";
+								return "a = 0; b = 0";
 							case 1:
-								return "factor = 6; level = 2";
-							case 2:
-								return "factor = -100; level = 2";
-							case 3:
-								return "level = 3";
-							case 4:
-								return "level = 100; factor = 0; k = 1";
-							case 5:
-								return "level = 100; factor = 2; k = 11";
+								return "a = 1; b = 100";
 							default:
 								throw new ArgumentException("Данного пути не существует");
 						}
@@ -208,17 +145,9 @@
 						switch (pathId)
 						{
 							case 0:
-								return "level = 1";
+								return "a = 0; b = 100";
 							case 1:
-								return "factor = 6; level = 2";
-							case 2:
-								return "factor = -100; level = 2";
-							case 3:
-								return "level = 3";
-							case 4:
-								return "level = 100; factor = 0; k = 11";
-							case 5:
-								return "level = 100; factor = 2; k = 1";
+								return "a = 1; b = -100";
 							default:
 								throw new ArgumentException("Данного пути не существует");
 						}
@@ -239,14 +168,6 @@
 						switch (pathId)
 						{
 							case 0:
-								return "16";
-							case 1:
-								return "8";
-							case 2:
-								return "1";
-							case 3:
-								return "0";
-							case 4:
 								return "200";
 							default:
 								throw new ArgumentException("Данного пути не существует");
@@ -257,17 +178,9 @@
 						switch (pathId)
 						{
 							case 0:
-								return "16";
-							case 1:
-								return "8";
-							case 2:
-								return "1";
-							case 3:
-								return "0";
-							case 4:
 								return "200";
-							case 5:
-								return "50";
+							case 1:
+								return "100";
 							default:
 								throw new ArgumentException("Данного пути не существует");
 						}
@@ -277,17 +190,9 @@
 						switch (pathId)
 						{
 							case 0:
-								return "16";
-							case 1:
-								return "8";
-							case 2:
-								return "1";
-							case 3:
 								return "0";
-							case 4:
-								return "50";
-							case 5:
-								return "50";
+							case 1:
+								return "100";
 							default:
 								throw new ArgumentException("Данного пути не существует");
 						}
@@ -297,17 +202,9 @@
 						switch (pathId)
 						{
 							case 0:
-								return "16";
-							case 1:
-								return "8";
-							case 2:
-								return "1";
-							case 3:
-								return "0";
-							case 4:
 								return "200";
-							case 5:
-								return "50";
+							case 1:
+								return "-100";
 							default:
 								throw new ArgumentException("Данного пути не существует");
 						}
@@ -321,50 +218,12 @@
 
 		public override string ToString()
 		{
-			return @"01 int j=2; //<TEST_EXAMPLE>
-02 switch (level)
+			return @"01 int a, b; //<TEST_EXAMPLE>
+02 if (a == 0 & b>10)
    {
-       case 1:
-       {
-03         for (int i = 1; i <= 5; i++)
-           {
-04             level= level+ i;
-           }
-05         return level;
-       }
-      case 2:
-      {
-06        Console.WriteLine(""Enter factor\"");
-07        if (factor==6)
-          {
-08            level=level*4;
-09            return level;
-          }
-          else
-          {
-10            level=level/2;
-11            return level;
-          }         
-      }
-      case 3:
-      {
-12        while (j > 0)
-          {
-13            level= level-j;
-14            j--;
-          }
-15        return level*3;
-        }
-      default :
-      {
-16        if (factor==0 && k > 10)
-          {
-17            level=level*4;
-          }          
-18            return level/2;
-      }
+03     b=b*2;
    }
-19 Console.ReadLine();";
+04 return b;";
 		}
 
 		private IBidirectionalGraph<object, IEdge<object>> GetGraphByPathVertexes(params int[] args)
---
+++ Test/control_exmpl - 4 criteries - Small.cs
@@ -0,0 +1,6 @@
+int  a, b; //<TEST_EXAMPLE>
+if (a == 0 & b>10)
+{
+    b=b*2;
+}
+return b;
